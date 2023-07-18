ca7be370050d74c21756cf664c8c3a9e467eede7
arsham-aazami <arsham20302040aazami@gmail.com>
2023-07-18 22:44:34 +0330

    building and implementing tabletsize widget

---
+++ lib/responsive/tablet_size.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class tabletSize extends StatelessWidget {
+  const tabletSize({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tablet Size"),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(205, 28, 112, 164),
+      ),
+      body: Column(children: [
+            Center(
+              child: Container(
+                color: Color.fromARGB(205, 28, 112, 164),
+                width: 300,
+                height: 20,
+               ),
+            ),
+            ListView.builder(
+              itemCount: 8,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Container(
+                    color: Colors.blueGrey, width: 300, height: 20,
+                  ),
+                );
+              },
+            )
+          ]),
+    );
+  }
+}
