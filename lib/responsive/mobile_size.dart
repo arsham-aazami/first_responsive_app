e6d1aaf456a3b8884a5df46b40ce1f72f362784a
arsham-aazami <arsham20302040aazami@gmail.com>
2023-07-19 12:26:36 +0330

    updating the mobile size widget

--- lib/responsive/mobile_size.dart
+++ lib/responsive/mobile_size.dart
@@ -6,34 +6,47 @@ class MobileSize extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Color.fromARGB(205, 28, 112, 164),
-            title: Text("Mobile Size"),
-            centerTitle: true,
-          ),
-          body: Column(children: [
-            Center(
-              child: Container(
-                child: Text("You are now in mobile mode", style: TextStyle(fontSize: 10),),
-                color: Color.fromARGB(205, 28, 112, 164),
-                width: 100,
-                height: 30,
-               ),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(205, 28, 112, 164),
+        title: Text("Mobile Size"),
+        centerTitle: true,
+      ),
+      body: Column(children: [
+        Center(
+          child: Container(
+            color: const Color.fromARGB(205, 28, 112, 164),
+            width: 100,
+            height: 40,
+            child: const Text(
+              "You are now in mobile mode",
+              style: TextStyle(fontSize: 10),
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 15,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Container(
-                      color: Colors.blueGrey, width: 10, height: 50,
-                    ),
-                  );
-                },
-              ),
-            )
-          ]),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: Container(
+            color: Color.fromARGB(205, 28, 112, 164),
+            width: 542,
+            height: 200,
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: 15,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  color: Color.fromARGB(205, 28, 112, 164),
+                  width: 100,
+                  height: 50,
+                ),
+              );
+            },
+          ),
+        )
+      ]),
     );
   }
 }
