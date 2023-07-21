3b05697edc1a387c0b176b497bed902e0df1797f
arsham-aazami <arsham20302040aazami@gmail.com>
2023-07-21 21:22:53 +0330

    adding normal font

--- lib/responsive/mobile_size.dart
+++ lib/responsive/mobile_size.dart
@@ -9,7 +9,7 @@ class MobileSize extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(205, 28, 112, 164),
         title: Image.network("https://th.bing.com/th/id/R.a9bb130db07da8a3faa41434465f5ba5?rik=4y3yRkL397fiAg&pid=ImgRaw&r=0", width: 200, height: 60,),
-        // centerTitle: true,
+        centerTitle: true,
       ),
       body: Column(children: [
         Expanded(
